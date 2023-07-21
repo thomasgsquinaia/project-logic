e53d3e26303095b488e6eb333f43b2522d16fb71
thomasgsquinaia <thomasquinaia@gmail.com>
2023-07-20 22:33:39 -0300

    commit de tuesday

--- c/logic011.c
+++ c/logic011.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+//12 - Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
 int main() {
   float altura, sum;
 
---
+++ c/logic012.c
@@ -0,0 +1,8 @@
+//013 -Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, 
+//utilizando as seguintes fórmulas: Para homens: (72.7h) - 58 Para mulheres: (62.1h) - 44.7 
+
+#include <stdio.h>
+
+int main() {
+  
+}
