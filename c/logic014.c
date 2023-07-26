75a0de4de8590f1867206626b5e8c6f46dfc38b2
thomasgsquinaia <thomasquinaia@gmail.com>
2023-07-26 17:22:51 -0300

    up de wednesday salariov2

---
+++ c/logic014.c
@@ -0,0 +1,18 @@
+//15 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+//Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda,
+//8% para o INSS e 5% para o sindicato, faça um programa que nos dê: salário bruto. quanto pagou ao INSS. 
+//quanto pagou ao sindicato. o salário líquido. calcule os descontos e o salário líquido, conforme a tabela abaixo:
+
+#include <stdio.h>
+
+int main() {
+  float horasTrabalhadasMes, salarioPorHora, somaSalarioMes;
+
+  printf("Digite o valor da sua hora: ");
+  scanf("%f", &salarioPorHora);
+
+
+
+
+}
+
