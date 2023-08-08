80c84d2cd9668caee28fc2da49ca30ad837e3d74
thomasgsquinaia <thomasquinaia@gmail.com>
2023-08-08 17:30:04 -0300

    [main] up tursday tempo de download de arquivo

--- c/logic017.c
+++ c/logic017.c
@@ -4,9 +4,17 @@
 #include <stdio.h>
 
 int main() {
-  float sizeFile, speedInternet; 
-  printf("digite o tamanhoo do arquivo para download: ");
+  float sizeFile, speedInternet, timeOfDownload, timeMinute; 
+  printf("digite o tamanho do arquivo para download: ");
   scanf("%f", &sizeFile);
-  printf("ola")
+  printf("Digite a velocidade da internet em Mbps(Mega bits por segungo):");
+  scanf("%f", &speedInternet);
+
+  // sizeFile recebe em MB e devemos passar pra MegaBits(Mb).
+  timeOfDownload = (sizeFile*8.0) / speedInternet;//o 8 é o o calculo passado pra megabits
+  timeMinute = timeOfDownload/60;
+  printf("O tempo aproximado de download é de %2.f segundos, %2.f minutos\n", timeOfDownload, timeMinute);
+
+
   return 0;
 }
