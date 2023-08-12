1e1e8f5fc1e3742260ab16335705ad0260fdb5d2
thomasgsquinaia <thomasquinaia@gmail.com>
2023-08-12 01:09:11 -0300

    [main] main: arrumando logic 18 num maior com while

--- c/logic018.c
+++ c/logic018.c
@@ -6,26 +6,26 @@ int main() {
   float n1, n2;
   int n;
   
-  n = 1;
-  while (n != 0) {
-      printf(" =========== Digite o número 0 para parar após digitar dois numeros ou mais!! ===========\n");
+  n1 = 1;
+  while (n1 != 0) {
+      printf(" =========== Digite o número zero (0) para parar o programa!! ===========\n");
       printf("Digite um número: \n");
       scanf("%f", &n1);
       printf("Digite outro número: \n");
       scanf("%f", &n2);
 
     if(n1 > n2 && n2 < n1) {
-      printf("O primeiro número %2.f é o maior número digitado!", n1);
-      printf(" =========== Digite o número 0 para parar após digitar dois numeros ou mais!! ===========\n");
+      printf("O primeiro número (%2.f) é o maior número digitado!\n", n1);
+      // printf(" =========== Digite o número 0 para parar após digitar dois numeros ou mais!! ===========\n");
 
     }
-
     if(n1 < n2 && n2 > n1) {
-      printf("O segundo número %2.f é o maior número digitado!", n2);
-      printf(" =========== Digite o número 0 para parar após digitar dois numeros ou mais!! ===========\n");
+      printf("O segundo número (%2.f) é o maior número digitado!\n", n2);
+      // printf(" =========== Digite o número 0 para parar após digitar dois numeros ou mais!! ===========\n");
     }
-    n++;
   }
+  printf("Fim");
+
 
   // if(n1 == n2) { 
   //   printf("Os números são iguais");
