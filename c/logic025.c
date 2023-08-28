1512efc678cf7fa3523054e7541c030851959f07
thomasgsquinaia <thomasquinaia@gmail.com>
2023-08-28 16:42:47 -0300

    [main] main: up of monday melhor produto

--- c/logic025.c
+++ c/logic025.c
@@ -1,8 +1,31 @@
-/*Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, 
+/*Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar,
 sabendo que a decisão é sempre pelo mais barato.
 */
 #include <stdio.h>
 
-int main() {
+int main()
+{
+  float p1, p2, p3;
 
+  printf("Digite o primeiro preço: ");
+  scanf("%f", &p1);
+  printf("Digite o segundo preço: ");
+  scanf("%f", &p2);
+  printf("Digite o terceiro preço: ");
+  scanf("%f", &p3);
+
+  if (p1 < p2 && p1 < p3)
+  {
+    printf("Produto mais barato é o %2.f", p1);
+  }
+  if (p2 < p1 && p2 < p3)
+  {
+    printf("Produto mais barato é o %2.f", p2);
+  }
+  if (p3 < p1 && p3 < p2)
+  {
+    printf("Produto mais barato é o %2.f", p3);
+  }
+
+  return 0;
 }
