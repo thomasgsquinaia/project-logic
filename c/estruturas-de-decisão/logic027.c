4ad14292b301f0be999baf9bb91da23084860ce2
thomasgsquinaia <thomasquinaia@gmail.com>
2023-08-29 20:46:02 -0300

    [main] main: up de wednesday bora bom dia boa tarde boa noite

--- c/estruturas-de-decisão/logic027.c
+++ c/estruturas-de-decisão/logic027.c
@@ -2,3 +2,27 @@
 28-Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
 Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
 */
+
+#include <stdio.h>
+
+int main() {
+  char turno;
+
+  printf("Digite o turno que você estuda: M-matutino ou V-Vespertino ou N-Noturno: ");
+  scanf("%c", &turno);
+
+  if(turno == 'M' || turno == 'm') {
+    printf("Bom Dia!");
+  }
+  else if(turno == 'V' || turno == 'v') {
+    printf("Boa Tarde!");
+  }
+  else if(turno == 'N' || turno == 'n') {
+    printf("Boa Noite!");
+  }
+  else {
+    printf("Valor Inválido!");
+  }
+
+  return 0;
+}
