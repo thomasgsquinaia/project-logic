dae7007bfd9598f2c77c7abc211ee806ad5bfb60
thomasgsquinaia <thomasquinaia@gmail.com>
2023-08-31 22:46:43 -0300

    [main] main: up of thursday salario

--- c/estruturas-de-decisão/logic028.c
+++ c/estruturas-de-decisão/logic028.c
@@ -14,3 +14,42 @@ informe na tela:
     o valor do aumento;
     o novo salário, após o aumento.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    float salario, percent_aumento, diferencaSalarial, salarioAtual;
+
+    printf("Digite o salário do colaborador: ");
+    scanf("%f", &salario);
+
+    if (salario <= 280)
+    {
+        percent_aumento = 20;
+    }
+    else if (salario > 280 && salario <= 700)
+    {
+        percent_aumento = 15;
+    }
+    else if (salario >= 700 && salario <= 1500)
+    {
+        percent_aumento = 10;
+    }
+    else if (salario >= 1500)
+    {
+        percent_aumento = 5;
+    }
+    else {
+        printf("Deu ruim os ifs acima.");
+    }
+
+    diferencaSalarial= salario * (percent_aumento / 100);
+    salarioAtual = salario + diferencaSalarial;
+    printf("O salário antes do reajuste: %2.f\n", salario);
+    printf("O percentual de aumento aplicado: %2.f%%\n", percent_aumento);
+    printf("O valor do aumento: R$%2.f\n", diferencaSalarial);
+    printf("O novo salário, após o aumento: %2.f\n", salarioAtual);
+
+    return 0;
+}
