327a8a366854c5d673c403e64826bf29ce31cf46
thomasgsquinaia <thomasquinaia@gmail.com>
2023-09-06 18:31:06 -0300

    [main] main: conta de imposto de renda up of wednesday

--- c/estruturas-de-decisão/logic029.c
+++ c/estruturas-de-decisão/logic029.c
@@ -1,10 +1,13 @@
 /*
 30 - Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda,
- que depende do salário bruto (conforme tabela abaixo) e 10% para o INSS e que o FGTS corresponde a 11% do Salário Bruto, 
+ que depende do salário bruto (conforme tabela abaixo) e 10% para o INSS e que o FGTS corresponde a 11% do Salário Bruto,
  mas não é descontado (é a empresa que deposita).
-O Salário Líquido corresponde ao Salário Bruto menos os descontos. 
+
+O Salário Líquido corresponde ao Salário Bruto menos os descontos.
+
 O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
-Desconto do IR: Salário Bruto até 900 (inclusive) - isento Salário Bruto até 1500 (inclusive) - 
+
+Desconto do IR: Salário Bruto até 900 (inclusive) - isento Salário Bruto até 1500 (inclusive) -
 desconto de 5% Salário Bruto até 2500 (inclusive) - desconto de 10% Salário Bruto acima de 2500 - desconto de 20%
 Imprima na tela as informações, dispostas conforme o exemplo abaixo.
 No exemplo o valor da hora é 5 e a quantidade de hora é 220.
@@ -16,3 +19,18 @@ No exemplo o valor da hora é 5 e a quantidade de hora é 220.
     Total de descontos              : R$  165,00
     Salário Liquido                 : R$  935,00
 */
+#include <stdio.h>
+
+int main()
+{
+    float valorHora, qtdHorasMes;
+
+    printf("Digite o valor da sua hora:")
+    scanf("%f", &valorHora);
+    printf("Digite a quantidade de horas que você trabalhou no mês:");
+    scanf("%f", &qtdHorasMes);
+
+    //contas
+
+    return 0;
+}
